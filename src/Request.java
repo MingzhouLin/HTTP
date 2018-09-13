b3bd58f5e86a50c9a63c95ff50a9e6fd8f7976a6
林铭洲 <linmingzhou@hayashis-MacBook-puro.local>
2018-09-12 21:42:59 -0400

    Supplement redirection and refactored the structure.

--- src/Request.java
+++ src/Request.java
@@ -1,7 +1,10 @@
+import java.util.Set;
+
 public class Request {
     public String host;
     public String content;
-
+    //This is for additional condition such as -v -d -h ...
+    public Set<String> addition;
     public Request() {
     }
 
